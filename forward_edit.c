e479b0f34094a19876c7215d9a05ca2eefa43b5e
oynx0601 <nouyang@umich.edu>
2017-08-06 22:58:28 -0400

    Update forward_edit.c

--- forward_edit.c
+++ forward_edit.c
@@ -6,11 +6,8 @@
 #include "const.h" 
 #include "logmath.h"
 
-//XXX: use log add or scaling factors???
-//didn't include silence state
 
-
-void ForwardMultiSeq(HMM *phmm, int T, int *O1, double *O2, double **alpha, double *pprob, int P, int *peakPos)
+void ForwardMultiSeq(HMM *phmm, int T, int *O1, double *O2, double *S, double **alpha, double *pprob, int P, int *peakPos)
 {
   int i, j, k;   /* state indices */
   int t;      /* time index */
@@ -24,14 +21,15 @@ void ForwardMultiSeq(HMM *phmm, int T, int *O1, double *O2, double **alpha, doub
         alpha[peakPos[k]][i] = LOGZERO;
       } 
       else {
-        alpha[peakPos[k]][i] = log(phmm->pi[i] * ComputeEmission(phmm, i, peakPos[k], O1, O2));
+        alpha[peakPos[k]][i] = log(phmm->pi[i] * ComputeEmission(phmm, i, peakPos[k], S, O2));
       }
-    }
+   }
   
 /* 2. Induction */
  
     for (t = peakPos[k]; t < peakPos[k+1]-1; t++) {
       for (j = 1; j <= phmm->N; j++) {
+        //fprintf(stdout, "%d,%d\t", t,j);
         sum = LOGZERO;
         for (i = 1; i <= phmm->N; i++) {
           if (alpha[t][i] != LOGZERO && log(phmm->A[i][j])!= LOGZERO) {
@@ -43,27 +41,37 @@ void ForwardMultiSeq(HMM *phmm, int T, int *O1, double *O2, double **alpha, doub
             }
           }
         }
-      
-        alpha[t+1][j] = sum + log(ComputeEmission(phmm, j, (t+1), O1, O2));
+        alpha[t+1][j] = sum + log(ComputeEmission(phmm, j, (t+1), S, O2));
       }
     }
-
 /* 3. Termination */
     pprob[k] = LOGZERO;
     for (i = 1; i <= phmm->N; i++) {
-      if (alpha[peakPos[k+1]-1][i] != LOGZERO) {
+      if (alpha[peakPos[k+1]-1][i] != LOGZERO && alpha[peakPos[k+1]-1][i] == alpha[peakPos[k+1]-1][i]) {
         if (pprob[k] != LOGZERO) {
           pprob[k] = logadd(pprob[k], alpha[peakPos[k+1]-1][i]);
         }
         else {
-		      pprob[k] = alpha[peakPos[k+1]-1][i];
+          pprob[k] = alpha[peakPos[k+1]-1][i];
         }
       }
+      
     }
   }
 }
 
+double ComputeEmission(HMM *phmm, int j, int t, double *S, double *O2) {
+  double pNorm;    /* multivariance normal distribution */ 
 
+  pNorm = (1/((pow(SQRT_TWO_PI, 2.0) * phmm->sigma[j][1] * phmm->sigma[j][2]) * pow((1 - pow(phmm->rho[j], 2.0)), 0.5))) * 
+	  exp( (-0.5) / (1 - pow(phmm->rho[j], 2.0))*(pow((S[t] - phmm->mu[j][1]), 2.0)/pow(phmm->sigma[j][1],2.0)+
+	  pow((O2[t] - phmm->mu[j][2]), 2.0)/pow(phmm->sigma[j][2],2.0)-2*phmm->rho[j]*(S[t] - phmm->mu[j][1])*(O2[t] - 
+	  phmm->mu[j][2])/(phmm->sigma[j][1]*phmm->sigma[j][2]))); 
+  //fprintf(stdout, "pNorm: %lf \t", pNorm);  
+  return pNorm;
+}
+
+/*
 double ComputeEmission(HMM *phmm, int j, int t, int *O1, double *O2) {
   double pNorm;    // P(O2) ~ N(mu, gamma) 
   if (j <= phmm->D){ //first D states are TF motif
@@ -77,3 +85,4 @@ double ComputeEmission(HMM *phmm, int j, int t, int *O1, double *O2) {
   }
   
 }
+*/
