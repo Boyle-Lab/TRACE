d4194abb62526f31f5dbe10f450b76667f25aa98
oynx0601 <nouyang@umich.edu>
2017-08-06 22:54:37 -0400

    Update esthmm_edit.c

--- esthmm_edit.c
+++ esthmm_edit.c
@@ -14,55 +14,52 @@ void Usage(char *name);
 int main (int argc, char *argv[])
 {
   
-	int T;
+  int T;
   double *GC;
-	HMM  	hmm;
-	int	N;
-	int	M;
-	double 	**alpha; 
-	double	**beta;
-	double	**gamma;
-	double	*O2; /* slop */
+  HMM  	hmm;
+  int	N;
+  int	M;
+  double  **alpha; 
+  double  **beta;
+  double  **gamma;
+  double  *O2; /* slop */
   int *O1; /* sequence, represented by number, 1=A, 2=C, 3=G, 4=T */
   int P; /* total number of peaks */
   int *peakPos;
-	//int	iflg=0, sflg=0, nflg=0, mflg=0, errflg =0, vflg=0;
-	int	c;
-	int	seed; /* seed for random number generator */
-	//char	*hmminitfile;
-	int	niter;
-	double	logprobinit, logprobfinal;
-	FILE	*fp;
- 
-  
-	if (argc != 6 && argc != 5) {
+  int	c;
+  int	seed; /* seed for random number generator */
+  int	niter;
+  double  logprobinit, logprobfinal;
+  FILE	*fp;
+	
+  if (argc != 6 && argc != 5) {
 		Usage(argv[0]);
 		exit (1);
-	}
-	/* read the observed sequence */
-	fp = fopen(argv[1], "r");
-	if (fp == NULL) {
-		fprintf(stderr, "Error: File %s not found \n", argv[1]);
-		exit (1);
-	}
-	ReadSequence(fp, &T, &GC, &O1, &P, &peakPos); 
-	fclose(fp);
+  }
+  /* read the observed sequence */
+  fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Error: File %s not found \n", argv[1]);
+    exit (1);
+  }
+  ReadSequence(fp, &T, &GC, &O1, &P, &peakPos); 
+  fclose(fp);
  
   fp = fopen(argv[2], "r");
-	if (fp == NULL) {
-		fprintf(stderr, "Error: File %s not found \n", argv[2]);
-		exit (1);
-	}
-	ReadSlop(fp, &T, &O2); 
-	fclose(fp);
+  if (fp == NULL) {
+    fprintf(stderr, "Error: File %s not found \n", argv[2]);
+    exit (1);
+  }
+  ReadSlop(fp, &T, &O2); 
+  fclose(fp);
   
 	/* initialize the hmm model */
-  fp = fopen(argv[3], "r");
-	if (fp == NULL) {
+  fp = fopen(argv[3], "r");	
+  if (fp == NULL) {
     fprintf(stderr, "Error: File %s not found \n", argv[3]);
-   	exit (1);
-	}
-  
+     exit (1);
+  }
+  /*
   if (argc == 6) {
 	  ReadHMM(fp, &hmm);
     fclose(fp);
@@ -70,10 +67,14 @@ int main (int argc, char *argv[])
     if (&seed == NULL) {
       fprintf(stderr, "Error: seed %s not valid \n", argv[4]);
       fprintf(stdout, "T: %d \n", T);
+      //fprintf(stdout, "D: %d \n", hmm.D);
+      //fprintf(stdout, "N: %d \n", hmm.N);
+      //fprintf(stdout, "GC: %lf \n", GC[1]);
    	  exit (1);
 	  }
     InitHMMwithInput(&hmm, seed, GC);
   }
+  */
   if (argc == 5) {
     ReadOutHMM(fp, &hmm);
     fclose(fp);
@@ -85,44 +86,36 @@ int main (int argc, char *argv[])
     printf("PROB");
   }
   PrintHMM(stdout, &hmm);
-	/* allocate memory */
-	alpha = dmatrix(1, T, 1, hmm.N);
-	beta = dmatrix(1, T, 1, hmm.N);
-	gamma = dmatrix(1, T, 1, hmm.N);
-	/* call Baum Welch */
-  BaumWelchMultiSeq(&hmm, T, O1, O2, alpha, beta, gamma, &niter, 
-		P, peakPos);
+  //fprintf(stdout, "N: %d \n", hmm.N);
+  /* allocate memory */
+  alpha = dmatrix(1, T, 1, hmm.N);
+  beta = dmatrix(1, T, 1, hmm.N);
+  gamma = dmatrix(1, T, 1, hmm.N);
+  /* call Baum Welch */
+  BaumWelchBVN(&hmm, T, O1, O2, alpha, beta, gamma, &niter, P, peakPos);
 
-	/* print the answer */
-	PrintHMM(stdout, &hmm);
+  /* print the answer */
+  PrintHMM(stdout, &hmm);
   fp = fopen(argv[argc-1], "w");
   if (fp == NULL) {
     fprintf(stderr, "Error: File %s not valid \n", argv[argc-1]);
-   	exit (1);
-	}
+     exit (1);
+  }
   PrintHMM(fp, &hmm);
   
-	/* free memory */
-	free_ivector(O1, 1, T);
-  //fprintf(stdout, "free1");
+  /* free memory */
+  free_ivector(O1, 1, T);
   free_dvector(O2, 1, T);
-  //fprintf(stdout, "free2");
-	free_dmatrix(alpha, 1, T, 1, hmm.N);
-  //fprintf(stdout, "free3");
-	free_dmatrix(beta, 1, T, 1, hmm.N);
-  //fprintf(stdout, "free4");
-	free_dmatrix(gamma, 1, T, 1, hmm.N);
-  //fprintf(stdout, "free5");
-	FreeHMM(&hmm);
+  free_dmatrix(alpha, 1, T, 1, hmm.N);
+  free_dmatrix(beta, 1, T, 1, hmm.N);
+  free_dmatrix(gamma, 1, T, 1, hmm.N);
+  FreeHMM(&hmm);
 }
 
 void Usage(char *name)
 {
 	printf("Usage error. \n");
-        /* printf("Usage1: %s [-v] -N <num_states> -M <num_symbols> <file.seq>\n", 
-		name);
-        printf("Usage2: %s [-v] -S <seed> -N <num_states> -M <num_symbols> <file.seq>\n", 
-		name); */
+        
         printf("Usage1: ./esthmm <file.seq> <file.slop> <mod.hmm> <seed> <out.hmm>\n");
         printf("Usage2: ./esthmm <file.seq> <file.slop> <mod.hmm> <out.hmm>\n");
         
@@ -130,5 +123,4 @@ void Usage(char *name)
         printf("  mod.hmm - file with the initial model parameters\n");
         printf("  file.seq - file containing the obs. seqence\n");
         printf("  file.slop - file containing the obs. slop\n");
-	//printf("  v - prints out number of iterations and log prob\n");
 }
