668a4248d91e9f3f1497d8479962639f697ab106
oynx0601 <nouyang@umich.edu>
2017-08-06 23:19:47 -0400

    Update viterbi_edit.c

--- viterbi_edit.c
+++ viterbi_edit.c
@@ -5,23 +5,42 @@ static char rcsid[] = "$Id: viterbi.c,v 1.1 1999/05/06 05:25:37 kanungo Exp kanu
 
 #define VITHUGE  100000000000.0
 
-void ViterbiMultiSeq(HMM *phmm, int T, int *O1, double *O2, double **delta, int **psi,
+void ViterbiMultiSeq(HMM *phmm, int T, int *O1, double *O2, double *S, double *g, double **delta, int **psi,
         int *q, double *vprob, double *pprob, int P, int *peakPos)
 {
-        int     i, j, k;   /* state indices */
-        int     t;      /* time index */
+  int     i, j, k;   /* state indices */
+  int     t;      /* time index */
  
-        int     maxvalind;
-        double  maxval, val;
+  int     maxvalind;
+  double  maxval, val;
 	double  **biot;
+ 
+  int	l = 0;
+  double  *logprobf, *logprobb, plogprobinit, plogprobfinal;
+  t = 0;
+  double  **alpha; 
+  double	**beta;
+  double	**gamma;
+ 
+  alpha = dmatrix(1, T, 1, phmm->N);
+  beta = dmatrix(1, T, 1, phmm->N);
+  gamma = dmatrix(1, T, 1, phmm->N);
+ 
+  logprobf = logprobb = dvector(1,P);
+  
+  ForwardMultiSeq(phmm, T, O1, O2, S, alpha, logprobf, P, peakPos);
+  
+  BackwardMultiSeq(phmm, T, O1, O2, S, beta, logprobb, P, peakPos);
+  
+  ComputeGammaWithLog(phmm, T, alpha, beta, gamma);
+  
+/* 0. Preprocessing */
 
-	/* 0. Preprocessing */
-
-	biot = dmatrix(1, phmm->N, 1, T);
-	for (i = 1; i <= phmm->N; i++) {
-		for (t = 1; t <= T; t++) {
-			biot[i][t] = log(ComputeEmission(phmm, i, t, O1, O2));
-		}
+  biot = dmatrix(1, phmm->N, 1, T);
+  for (i = 1; i <= phmm->N; i++) {
+    for (t = 1; t <= T; t++) {
+      biot[i][t] = log(ComputeEmission(phmm, i, t, S, O2));
+    }
   } 
   for (k = 1; k <= P; k++){
     /* 1. Initialization  */     
@@ -57,16 +76,16 @@ void ViterbiMultiSeq(HMM *phmm, int T, int *O1, double *O2, double **delta, int 
         q[peakPos[k+1]-1] = i;
       }
     }
+    g[peakPos[k+1]-1] = gamma[peakPos[k+1]-1][q[peakPos[k+1]-1]];
     vprob[peakPos[k+1]-1] = *pprob; 
 	  /* 4. Path (state sequence) backtracking */
 
   	for (t = peakPos[k+1] - 2; t >= peakPos[k]; t--){
 	  	q[t] = psi[t+1][q[t+1]];
       vprob[t] = delta[t+1][q[t+1]];
+      g[t] = gamma[t][q[t]];
     }
   }
+
 }
- 
-
-
   
